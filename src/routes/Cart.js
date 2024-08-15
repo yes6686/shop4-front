390ee2101841c9063111e8ee492b6174cada8590
안예찬 <yes490411@gmail.com>
2024-08-15 18:44:54 +0900

    장바구니 수정

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Table } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import {addCount} from './../store/cartSlice'
+import './../App.css'
 
 const Cart = () => {
 
     let state = useSelector((state)=> { return state})
     let dispatch = useDispatch()
+    const [number, setNumber] = useState(0); // 초기 숫자값을 0으로 설정
+
+    const increaseNumber = () => {
+        setNumber(prevNumber => prevNumber + 1); // 숫자 증가
+    };
+
+    const decreaseNumber = () => {
+        setNumber(prevNumber => (prevNumber > 0 ? prevNumber - 1 : 0)); // 숫자 감소, 0보다 작아지지 않도록
+    };
+ 
     
     return (
         <div>
@@ -15,10 +26,12 @@ const Cart = () => {
         <thead>
           <tr>
             <th>#</th>
-            <th>상품명</th>
-            <th>가격</th>
+            <th>이미지</th>
+            <th>상품정보</th>
             <th>수량</th>
-            <th>변경</th>
+            <th>상품구매금액</th>
+            <th>배송비</th>
+            <th>선택</th>
           </tr>
         </thead>
         <tbody>
@@ -27,8 +40,17 @@ const Cart = () => {
                 <>
                 <tr key={i}>
                 <td>{state.cart[i].id}</td>
+                <td><img className="image" src={`${state.cart[i].url}`}></img></td>
                 <td>{state.cart[i].name}</td>
+                <td>
+                <div >
+                    <div onClick={increaseNumber}>&#8593;</div> {/* 위쪽 화살표 */}
+                    <div>{number}</div>
+                    <div onClick={decreaseNumber}>&#8595;</div> {/* 아래쪽 화살표 */}
+                </div>
+                </td>
                 <td>{state.cart[i].price}</td>
+                <td>무료</td>
                 <td>{state.cart[i].stock}</td>
                 <td>
                     <button onClick={()=>{
@@ -45,5 +67,7 @@ const Cart = () => {
       </div>
     )
 }
+
+
 
 export default Cart
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -4,15 +4,6 @@ let cart = createSlice({
     name : 'item',
     initialState : [
         // 장바구니 상품
-        {
-            "id": 1,
-            "category": "Shoes",
-            "description": "Born in France",
-            "name": "White and Black",
-            "price": 120000,
-            "stock": 2,
-            "url": "https://codingapple1.github.io/shop/shoes1.jpg"
-        }
     ],
     reducers : {
         addCount(state,action){ // 버튼 누른 id와 같은 id값을 갖는 state의 stock 개수 증가
