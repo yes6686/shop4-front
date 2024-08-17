01c77a2b87d2b3dbffd1f54e2ca8d8f90c9e7944
안예찬 <yes490411@gmail.com>
2024-08-17 22:52:04 +0900

    유저이름 옆에 드롭다운 버튼 만들기

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,55 @@
-import './../App.css'
-import React from 'react'
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import './../App.css';
+import React from 'react';
+import { Navbar, Container, Nav, Dropdown } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-    const navigate = useNavigate()
-    let state = useSelector((state)=> { return state})
+    const navigate = useNavigate();
+    const state = useSelector((state) => state);
 
-    return(
-    <div className="App">
-    <Navbar bg="success" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand onClick={()=>{navigate('/')}}>Shop</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('/Cart')}}>Cart</Nav.Link>
-          </Nav>
-          {/* 사용자 이름 표시 */}
-          {state.user && (
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
+    return (
+        <div className="App">
+            <Navbar bg="success" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand onClick={() => navigate('/')}>Shop</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
+                        <Nav.Link onClick={() => navigate('/Cart')}>Cart</Nav.Link>
+                    </Nav>
+                    {/* 사용자 이름 표시 */}
+                    {state.user && (
                         <Nav className="ms-auto">
-                            <Navbar.Brand className="user-name" disabled>{state.user}</Navbar.Brand>
+                            <Dropdown>
+                                <Dropdown.Toggle
+                                    className="user-name"
+                                    variant="success"
+                                    id="dropdown-basic"
+                                    style={{ cursor: 'pointer' }}
+                                >
+                                    {state.user}
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu className="custom-dropdown-menu">
+                                    <Dropdown.Item onClick={() => handleNavigation('/mypage')}>
+                                        My Page
+                                    </Dropdown.Item>
+                                    <Dropdown.Item onClick={() => handleNavigation('/Cart')}>
+                                        Cart
+                                    </Dropdown.Item>
+                                    {/* You can add more items here */}
+                                </Dropdown.Menu>
+                            </Dropdown>
                         </Nav>
                     )}
-        </Container>
-    </Navbar>
-    </div>
+                </Container>
+            </Navbar>
+        </div>
     );
-    
 }
 
-export default Header
+export default Header;
