d95097f8d470c6f25307870b5b39d42237d0bca9
ok63477 <ok63477@gmail.com>
2024-08-18 20:28:49 +0900

    Describe your changes here

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,18 +1,48 @@
 import './Login.css'
-
+import {useState} from 'react'
+import axios from 'axios'
 
 function Login() {
   
+   
+
+
+
+  let [userId,setUserId] = useState();
+  let [userPw,setUserPw] = useState();
+
+  const userIdChange = (e) => setUserId(e.target.value);
+  const userPwChange = (e) => setUserPw(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:8080/api/members/login", {
+      userId : userId,
+      userPw : userPw
+    })
+    .then((res)=>{
+      console.log("로그인 요청 성공!");
+    })
+    .catch(function(error) {
+      console.log("로그인 요청 실패!");
+    })
+    
+    
+  }
+
+
+
   return (
       <>
-        <form className="memberForm">
+        <form className="memberForm" onSubmit={onSubmit} encType="multipart/form">
           
           <div className="mb-3">
-            <input type="text" className="form-control" id="exampleInputEmail1" placeholder="아이디"/>
+            <input type="text" value={userId} onChange={userIdChange} placeholder="아이디"/>
           </div>
           
           <div className="mb-3">
-            <input type="password" className="form-control" id="inputUserPw" placeholder="비밀번호"/>
+            <input type="password" value={userPw} onChange={userPwChange} placeholder="비밀번호"/>
           </div>
           
           <div className="mb-3 form-check">
@@ -23,14 +53,14 @@ function Login() {
             <label for="autoLogin">자동로그인</label>
           </div>
           
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" value="user login">Submit</button>
           
           <ul className="findRemove">
             <li>아이디/비밀번호 찾기</li>    
             <li>휴면해제</li>
             <li>회원가입</li>
           </ul>
-          
+
         </form>
         
         
