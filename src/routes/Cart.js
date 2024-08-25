680d2a5276bd0b9b394345267846911542326b5f
kimdonghyuk <kdh367036@naver.com>
2024-08-25 15:22:45 +0900

    FIX merge

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -21,6 +21,15 @@ const Cart = () => {
   const user = sessionStorage.getItem('user');
   const [checkGoods, setcheckGoods] = useState(new Set()); //Set 은 중복없이 유일한값만 저장하는 배열임,cartId가 저장됨 구매시 이용
   const [showAlert, setShowAlert] = useState(false);
+  const isLoggedIn = sessionStorage.getItem('isLoggedIn'); // 로그인 여부 확인
+
+  useEffect(() => {
+    console.log(user);
+    if (!isLoggedIn || !user) {
+      navigate('/login'); // 로그인되지 않았거나 사용자 정보가 없는 경우 로그인 페이지로 이동
+      return;
+    }
+  }, [isLoggedIn, user, navigate]);
 
   useEffect(() => {
     if (user) {
