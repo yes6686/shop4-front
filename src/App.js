402d96eecb99e22180157f907843b03220b8ab71
kimdonghyuk <kdh367036@naver.com>
2024-09-06 22:56:24 +0900

    FEAD 카카오로그인 프론트만 구현

--- src/App.js
+++ src/App.js
@@ -14,29 +14,31 @@ import Direct from './routes/Direct';
 import Login from './routes/Login';
 import SignUp from './routes/SignUp';
 import Payment from './routes/Payment';
+import LoginHandeler from './components/LoginHandeler';
 
 function App() {
-	return (
-		<>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/detail/:id" element={<Detail />} />
-				<Route path="/cart" element={<Cart />} />
-				<Route path="/myPage" element={<MyPage />} />
-				<Route path="/recentlyViewed" element={<RecentlyViewed />} />
-				<Route path="/direct" element={<Direct />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signUp" element={<SignUp />} />
-				<Route path="/payment" element={<Payment />} />
-			</Routes>
-			{/* footer 박스로 클릭안되는 현상 처리 */}
-			<br />
-			<br />
-			<br />
-			{/* <Footer /> */}
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/myPage" element={<MyPage />} />
+        <Route path="/recentlyViewed" element={<RecentlyViewed />} />
+        <Route path="/direct" element={<Direct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/login/kakao" element={<LoginHandeler />} />
+      </Routes>
+      {/* footer 박스로 클릭안되는 현상 처리 */}
+      <br />
+      <br />
+      <br />
+      {/* <Footer /> */}
+    </>
+  );
 }
 
 export default App;
