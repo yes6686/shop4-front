91949cae535e02158c7f1f14bf7825725485b22b
ok63477 <ok63477@gmail.com>
2024-09-12 13:35:20 +0900

    주석달기

--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -4,19 +4,22 @@ import { getMember, updateMember } from '../services/MemberService';
 import { deleteCart, getCart, listCarts } from '../services/CartService';
 import { updateGoods } from '../services/GoodsService';
 
+//결제 페이지
 function Payment() {
 	//유저정보, 유저정보의 id 키값으로 cart에서 상품들 받아옴
 	let userInfo = JSON.parse(sessionStorage.getItem('user'));
-	let [copyUser, setCopyUser] = useState([]);
-	let [cartData, setCartData] = useState([]);
-	const { state } = useLocation();
+	let [copyUser, setCopyUser] = useState([]); //유저 정보
+	let [cartData, setCartData] = useState([]); //장바구니 정보
+	const { state } = useLocation(); 
 	let products = Array.from(state);
 	let [flag, setFlag] = useState(0);
 
 	const id = userInfo.id;
+
 	//장바구니 상품들 총 결제금액 변수
 	let [totalPrice, setTotalPrice] = useState(0);
 
+	// user의 id값이 바뀔때마다 정보 갱신
 	useEffect(() => {
 		const initMembers = async () => {
 			await getMember(id).then((response) => {
@@ -26,6 +29,7 @@ function Payment() {
 		initMembers();
 	}, [id]);
 
+	// 장바구니 상품 목록 갱신(추가)
 	useEffect(() => {
 		const getCarts = async () => {
 			products.map(async (id) => {
@@ -37,6 +41,7 @@ function Payment() {
 		getCarts();
 	}, []);
 
+	// 결제 금액 계산 : 장바구니 목록 바뀔때마다 가격도 추가 계산
 	useEffect(() => {
 		let tmp = 0;
 		cartData.map((item) => {
