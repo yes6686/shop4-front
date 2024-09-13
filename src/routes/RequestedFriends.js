eb0a511a680f706afeefcf5800ba34d8620a58ef
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2024-09-14 00:18:22 +0900

    Revert "FEAT, FIX"

--- src/routes/RequestedFriends.js
+++ src/routes/RequestedFriends.js
@@ -3,11 +3,11 @@ import MyPageLeftSideBar from '../components/MyPageLeftSideBar';
 import './css/MyPage.css';
 
 function RequestedFriends() {
-	return (
-		<div className="myPageContainer">
-			<MyPageLeftSideBar />
-			<div className="rightContent">ddddddf</div>
-		</div>
-	);
+  return (
+    <div className="myPageContainer">
+      <MyPageLeftSideBar />
+      <div className="rightContent">ddddddf</div>
+    </div>
+  );
 }
 export default RequestedFriends;
