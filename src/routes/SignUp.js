ee0aee2f60ed1a68a0d81ace798d4a3be02143fc
kimdonghyuk <kdh367036@naver.com>
2024-09-18 23:24:56 +0900

    change local

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -1,20 +1,20 @@
-import "./css/SignUp.css";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { createMember } from "../services/MemberService";
-import axios from "axios";
+import './css/SignUp.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createMember } from '../services/MemberService';
+import axios from 'axios';
 
 function SignUp() {
   const navigate = useNavigate();
 
   // json형태로 변수 선언 후에 값 입력받기 (아이디, 비밀번호, 생년월일, 성별)
   const [formData, setFormData] = useState({
-    userId: "",
-    userPw: "",
-    name: "",
-    phone: "",
-    email: "",
-    birth: "",
+    userId: '',
+    userPw: '',
+    name: '',
+    phone: '',
+    email: '',
+    birth: '',
   });
 
   // 비밀번호 확인 변수
@@ -24,16 +24,16 @@ function SignUp() {
 
   // 중복 되는지 확인하는 변수
   const [isIdUnique, setIsIdUnique] = useState(false);
-  const [confirmPw, setConfirmPw] = useState(""); // 비밀번호 확인 상태 추가
+  const [confirmPw, setConfirmPw] = useState(''); // 비밀번호 확인 상태 추가
 
   // 생년월일 입력 필드를 별도로 관리
   const handleYearChange = (e) => {
     setYear(e.target.value);
     setFormData((prevState) => ({
       ...prevState,
-      birth: `${e.target.value}-${String(month).padStart(2, "0")}-${String(
+      birth: `${e.target.value}-${String(month).padStart(2, '0')}-${String(
         day
-      ).padStart(2, "0")}`,
+      ).padStart(2, '0')}`,
     }));
   };
 
@@ -41,9 +41,9 @@ function SignUp() {
     setMonth(e.target.value);
     setFormData((prevState) => ({
       ...prevState,
-      birth: `${year}-${String(e.target.value).padStart(2, "0")}-${String(
+      birth: `${year}-${String(e.target.value).padStart(2, '0')}-${String(
         day
-      ).padStart(2, "0")}`,
+      ).padStart(2, '0')}`,
     }));
   };
 
@@ -51,9 +51,9 @@ function SignUp() {
     setDay(e.target.value);
     setFormData((prevState) => ({
       ...prevState,
-      birth: `${year}-${String(month).padStart(2, "0")}-${String(
+      birth: `${year}-${String(month).padStart(2, '0')}-${String(
         e.target.value
-      ).padStart(2, "0")}`,
+      ).padStart(2, '0')}`,
     }));
   };
 
@@ -65,9 +65,9 @@ function SignUp() {
 
   // 빈칸있는지 확인
   const validateForm = () => {
-    console.log(Object.values(formData).every((value) => value.trim() !== ""));
+    console.log(Object.values(formData).every((value) => value.trim() !== ''));
     return (
-      Object.values(formData).every((value) => value.trim() !== "") &&
+      Object.values(formData).every((value) => value.trim() !== '') &&
       confirmPw === formData.userPw &&
       formData.phone.length === 11
     );
@@ -79,11 +79,11 @@ function SignUp() {
 
     // 빈칸 확인
     if (!validateForm()) {
-      alert("모든 필드를 채워주세요.");
+      alert('모든 필드를 채워주세요.');
       return;
     }
     if (!isIdUnique) {
-      alert("중복확인 해주세요");
+      alert('중복확인 해주세요');
       return;
     }
 
@@ -92,9 +92,9 @@ function SignUp() {
       const data = { ...formData, cash: 0 };
       console.log(data);
       await createMember(data);
-      alert("회원가입이 완료되었습니다.");
+      alert('회원가입이 완료되었습니다.');
     } catch (error) {
-      alert("회원가입에 실패하였습니다.");
+      alert('회원가입에 실패하였습니다.');
       console.error(error);
     }
   };
@@ -102,7 +102,7 @@ function SignUp() {
   //아이디 중복확인
   const checkDuplicate = async () => {
     if (formData.userId.length === 0) {
-      alert("아이디를 입력해주세요.");
+      alert('아이디를 입력해주세요.');
       return;
     }
     try {
@@ -110,21 +110,21 @@ function SignUp() {
         `http://localhost:8080/api/members/check/${formData.userId}`
       );
       if (res.data) {
-        alert("중복된 아이디입니다. 다른 아이디를 사용해주세요.");
+        alert('중복된 아이디입니다. 다른 아이디를 사용해주세요.');
         setIsIdUnique(false);
       } else {
-        alert("사용 가능한 아이디입니다.");
+        alert('사용 가능한 아이디입니다.');
         setIsIdUnique(true);
       }
     } catch (error) {
-      console.error("아이디 중복 확인에 실패했습니다.", error);
-      alert("서버에 문제가 발생했습니다. 나중에 다시 시도해주세요.");
+      console.error('아이디 중복 확인에 실패했습니다.', error);
+      alert('서버에 문제가 발생했습니다. 나중에 다시 시도해주세요.');
     }
   };
 
   //이메일 입력받기
-  const [emailPrefix, setEmailPrefix] = useState("");
-  const [domain, setDomain] = useState("gmail.com");
+  const [emailPrefix, setEmailPrefix] = useState('');
+  const [domain, setDomain] = useState('gmail.com');
   const [isCustomDomain, setIsCustomDomain] = useState(false); // 직접입력 여부 관리
 
   const handlePrefixChange = (e) => {
@@ -133,9 +133,9 @@ function SignUp() {
 
   const handleDomainChange = (e) => {
     const selectedDomain = e.target.value;
-    if (selectedDomain === "none") {
+    if (selectedDomain === 'none') {
       setIsCustomDomain(true); // 직접입력을 활성화
-      setDomain(""); // 빈 값으로 설정
+      setDomain(''); // 빈 값으로 설정
     } else {
       setIsCustomDomain(false); // 직접입력 비활성화
       setDomain(selectedDomain); // 선택한 도메인을 설정
@@ -150,9 +150,9 @@ function SignUp() {
   };
 
   // 입력받은 생년월일
-  const [year, setYear] = useState("");
-  const [month, setMonth] = useState("");
-  const [day, setDay] = useState("");
+  const [year, setYear] = useState('');
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
 
   // 연도, 월, 일 목록 생성
   const years = Array.from({ length: 100 }, (_, i) => 2024 - i); // 1924년부터 2024년까지
@@ -162,7 +162,7 @@ function SignUp() {
   return (
     <>
       <form className="signUpForm" onSubmit={onSubmit}>
-        <h2 className="title" style={{ marginTop: "20px" }}>
+        <h2 className="title" style={{ marginTop: '20px' }}>
           회원가입
         </h2>
 
@@ -170,7 +170,7 @@ function SignUp() {
         <label
           htmlFor="userId"
           className="form-label"
-          style={{ marginBottom: "0" }}
+          style={{ marginBottom: '0' }}
         >
           아이디
         </label>
@@ -182,20 +182,20 @@ function SignUp() {
             onChange={handleChange}
             className="form-control"
             placeholder="아이디"
-            style={{ height: "50px", paddingRight: "120px" }}
+            style={{ height: '50px', paddingRight: '120px' }}
           />
           <button
             type="button"
             className="btn btn-primary position-absolute"
             style={{
-              width: "120px",
-              height: "50px",
-              padding: "0",
-              fontSize: "0.875rem",
-              right: "0",
-              top: "50%",
-              transform: "translateY(-50%)",
-              borderRadius: "0",
+              width: '120px',
+              height: '50px',
+              padding: '0',
+              fontSize: '0.875rem',
+              right: '0',
+              top: '50%',
+              transform: 'translateY(-50%)',
+              borderRadius: '0',
             }}
             onClick={checkDuplicate}
           >
@@ -207,7 +207,7 @@ function SignUp() {
         <label
           htmlFor="pw"
           className="form-label"
-          style={{ marginBottom: "0" }}
+          style={{ marginBottom: '0' }}
         >
           비밀번호
         </label>
@@ -218,7 +218,7 @@ function SignUp() {
             onChange={handleChange}
             className="form-control"
             placeholder="비밀번호 입력(문자, 숫자, 특수문자 포함 8~20자)"
-            style={{ height: "50px" }}
+            style={{ height: '50px' }}
           />
         </div>
 
@@ -226,7 +226,7 @@ function SignUp() {
         <label
           htmlFor="confirmPw"
           className="form-label"
-          style={{ marginBottom: "0" }}
+          style={{ marginBottom: '0' }}
         >
           비밀번호 확인
         </label>
@@ -237,7 +237,7 @@ function SignUp() {
             onChange={handleConfirmPwChange}
             className="form-control"
             placeholder="비밀번호 재입력"
-            style={{ height: "50px" }}
+            style={{ height: '50px' }}
           />
         </div>
 
@@ -245,7 +245,7 @@ function SignUp() {
         <label
           htmlFor="name"
           className="form-label"
-          style={{ marginBottom: "0" }}
+          style={{ marginBottom: '0' }}
         >
           이름
         </label>
@@ -256,7 +256,7 @@ function SignUp() {
             onChange={handleChange}
             className="form-control"
             placeholder="이름을 입력해주세요."
-            style={{ height: "50px" }}
+            style={{ height: '50px' }}
           />
         </div>
 
@@ -264,7 +264,7 @@ function SignUp() {
         <label
           htmlFor="phone"
           className="form-label"
-          style={{ marginBottom: "0" }}
+          style={{ marginBottom: '0' }}
         >
           전화번호
         </label>
@@ -275,7 +275,7 @@ function SignUp() {
             onChange={handleChange}
             className="form-control"
             placeholder="휴대폰 번호 입력('-'제외 11자리 입력)"
-            style={{ height: "50px" }}
+            style={{ height: '50px' }}
           />
         </div>
 
@@ -283,12 +283,12 @@ function SignUp() {
         <label
           htmlFor="email"
           className="form-label"
-          style={{ marginBottom: "0" }}
+          style={{ marginBottom: '0' }}
         >
           이메일
         </label>
         <div className="input-container" name="email">
-          <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <input
               type="text"
               value={emailPrefix}
@@ -298,7 +298,7 @@ function SignUp() {
               }}
               placeholder="이메일 입력"
               className="form-control email-prefix"
-              style={{ height: "50px" }}
+              style={{ height: '50px' }}
             />
             <span className="at-sign">@</span>
             <input
@@ -310,7 +310,7 @@ function SignUp() {
               }}
               placeholder="도메인 선택 또는 직접 입력"
               className="form-control email-domain"
-              style={{ height: "50px" }}
+              style={{ height: '50px' }}
               disabled={!isCustomDomain}
             />
           </div>
