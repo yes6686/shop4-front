f66770fede86c9cbcf02a29d13feeb2797e861e4
kimdonghyuk <kdh367036@naver.com>
2024-09-19 14:46:02 +0900

    FEAT 이미지 데이터베이스 연동(바이너리코드)

--- src/services/MemberService.js
+++ src/services/MemberService.js
@@ -14,3 +14,14 @@ export const updateMember = (memberId, member) =>
 
 export const deleteMember = (memberId) =>
   axios.delete(REST_API_BASE_URL + '/' + memberId);
+
+export const updateProfileImage = (memberId, image) =>
+  axios.patch(REST_API_BASE_URL + '/updateProfileImage/' + memberId, image);
+
+export const getProfileImage = (memberId) =>
+  axios.get(REST_API_BASE_URL + '/getProfileImage/' + memberId, {
+    responseType: 'blob',
+  });
+
+export const deleteProfileImage = (memberId) =>
+  axios.delete(REST_API_BASE_URL + '/deleteProfileImage/' + memberId);
