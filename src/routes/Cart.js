b51e0a275967106e00195e586dee57ee190fcaba
ok63477 <ok63477@gmail.com>
2024-09-22 20:09:38 +0900

    Cart페이지 주석

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
-import './../App.css';
-import { useNavigate } from 'react-router-dom';
-import { FiShoppingCart } from 'react-icons/fi';
-import { AiOutlinePicture } from 'react-icons/ai';
-import { GiClothes } from 'react-icons/gi';
-import { FaMoneyBillWave } from 'react-icons/fa';
-import { PiPackageDuotone } from 'react-icons/pi';
-import { FaCartArrowDown } from 'react-icons/fa6';
-import { BsCartPlus, BsCartDash } from 'react-icons/bs';
-import { listCarts, updateCart } from '../services/CartService';
-import { MdOutlineDeleteForever } from 'react-icons/md';
-import { deleteCart } from '../services/CartService';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CheckBox from '../components/CheckBox';
-import ToastComponent from '../components/ToastComponent';
+import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
+import "./../App.css";
+import { useNavigate } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
+import { AiOutlinePicture } from "react-icons/ai";
+import { GiClothes } from "react-icons/gi";
+import { FaMoneyBillWave } from "react-icons/fa";
+import { PiPackageDuotone } from "react-icons/pi";
+import { FaCartArrowDown } from "react-icons/fa6";
+import { BsCartPlus, BsCartDash } from "react-icons/bs";
+import { listCarts, updateCart } from "../services/CartService";
+import { MdOutlineDeleteForever } from "react-icons/md";
+import { deleteCart } from "../services/CartService";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CheckBox from "../components/CheckBox";
+import ToastComponent from "../components/ToastComponent";
 
 //20240912 이전 -> Cart.js에서 cartId List(Set)만 넘기고 payment에서 cart를 불러왔음
 //-> detail에서 payment로 cartid만 완전히 개별적으로 넘길수가 없음
@@ -23,7 +23,7 @@ import ToastComponent from '../components/ToastComponent';
 const Cart = () => {
   let navigate = useNavigate();
   const [cartData, setCartData] = useState([]);
-  const user = sessionStorage.getItem('user');
+  const user = sessionStorage.getItem("user");
   const [checkGoods, setcheckGoods] = useState(new Set()); //Set 은 중복없이 유일한값만 저장하는 배열임,cartId가 저장됨 구매시 이용
 
   const [delivGoods, setDelivGoods] = useState([]);
@@ -31,6 +31,7 @@ const Cart = () => {
 
   let [toast, setToast] = useState(false);
 
+  // 세션에 user있을 시 json형태로 바꾸고 member_id 추출해서 getAllCart 호출
   useEffect(() => {
     if (user) {
       const userData = JSON.parse(user);
@@ -40,6 +41,7 @@ const Cart = () => {
     }
   }, []);
 
+  // 해당 member_id에 api요청
   async function getAllCart(member_id) {
     await listCarts(member_id)
       .then(async (response) => {
@@ -50,6 +52,7 @@ const Cart = () => {
       });
   }
 
+  // api요청 응답이 오면 장바구니 목록 중복되지 않게 셋팅
   async function setCarts() {
     const filterGoods = await Promise.all(
       cartData.filter((item) => checkGoods.has(item.id))
@@ -62,12 +65,12 @@ const Cart = () => {
   //아마 javascript 비동기 문제인듯
   useEffect(() => {
     if (delivGoods.length > 0) {
-      navigate('/payment', { state: delivGoods });
+      navigate("/payment", { state: delivGoods });
     }
   }, [delivGoods]);
 
+  // 수량증감 핸들러 (증가, 감소)
   const updateQuantity = (item, delta) => {
-    //수량증감 핸들러
     const newQuantity = item.quantity + delta;
     if (newQuantity > item.goods.stock) {
       setShowAlert(true);
@@ -102,7 +105,7 @@ const Cart = () => {
   };
 
   const iconStyle = {
-    fontSize: '24px',
+    fontSize: "24px",
   };
   return (
     <div>
@@ -114,54 +117,54 @@ const Cart = () => {
       <Table
         bordered
         hover
-        style={{ width: '100%', borderCollapse: 'collapse' }}
+        style={{ width: "100%", borderCollapse: "collapse" }}
       >
         <thead>
           <tr>
             <th
               style={{
-                textAlign: 'center',
-                verticalAlign: 'middle',
+                textAlign: "center",
+                verticalAlign: "middle",
               }}
             >
               <FaCartArrowDown style={iconStyle} />
             </th>
             <th
               style={{
-                textAlign: 'center',
-                verticalAlign: 'middle',
+                textAlign: "center",
+                verticalAlign: "middle",
               }}
             >
               <AiOutlinePicture style={iconStyle} />
             </th>
             <th
               style={{
-                textAlign: 'center',
-                verticalAlign: 'middle',
+                textAlign: "center",
+                verticalAlign: "middle",
               }}
             >
               <GiClothes style={iconStyle} />
             </th>
             <th
               style={{
-                textAlign: 'center',
-                verticalAlign: 'middle',
+                textAlign: "center",
+                verticalAlign: "middle",
               }}
             >
               <FiShoppingCart style={iconStyle} />
             </th>
             <th
               style={{
-                textAlign: 'center',
-                verticalAlign: 'middle',
+                textAlign: "center",
+                verticalAlign: "middle",
               }}
             >
               <FaMoneyBillWave style={iconStyle} />
             </th>
             <th
               style={{
-                textAlign: 'center',
-                verticalAlign: 'middle',
+                textAlign: "center",
+                verticalAlign: "middle",
               }}
             >
               <PiPackageDuotone style={iconStyle} />
@@ -170,13 +173,14 @@ const Cart = () => {
         </thead>
         <tbody>
           {cartData.map((item) => (
-            <tr key={item.id} style={{ textAlign: 'center', fontSize: '22px' }}>
+            <tr key={item.id} style={{ textAlign: "center", fontSize: "22px" }}>
               <td>
                 <CheckBox
                   id={item.id}
                   checkGoodsHandler={checkGoodsHandler} // props로 함수전달
                 />
               </td>
+              {/* 이미지 칸 클릭하면 디테일 페이지로 이동 */}
               <td
                 onClick={() => {
                   navigate(`/detail/${item.goods.id}`);
@@ -194,23 +198,24 @@ const Cart = () => {
               <td>무료</td>
               <td>
                 <button
-                  style={{ marginRight: '10px' }}
+                  style={{ marginRight: "10px" }}
                   onClick={() => updateQuantity(item, 1)}
                 >
                   <BsCartPlus />
                 </button>
                 <button
-                  style={{ marginRight: '10px' }}
+                  style={{ marginRight: "10px" }}
                   onClick={() => updateQuantity(item, -1)}
                 >
                   <BsCartDash />
                 </button>
                 <button
-                  style={{ marginRight: '10px' }}
+                  style={{ marginRight: "10px" }}
                   onClick={() => {
                     deleteCart(item.id);
                     let delete_id = item.id;
                     setCartData((prev) => {
+                      //delete_id 값만 제외한 상품들로 재구성
                       return prev.filter((goods) => goods.id !== delete_id);
                     });
                   }}
@@ -224,7 +229,7 @@ const Cart = () => {
       </Table>
       <div
         className="d-flex justify-content-center align-items-center"
-        style={{ height: '20vh' }}
+        style={{ height: "20vh" }}
       >
         <button
           className="buy-button"
@@ -248,7 +253,7 @@ const Cart = () => {
           setToast={setToast}
         ></ToastComponent>
       ) : (
-        ' '
+        " "
       )}
     </div>
   );
